712093ad9c9849f70208c3c1c584d7caa75e84a5
admin <admin@admin.com>
2025-11-04 01:01:18 +0800

    .

--- src/Facade.php
+++ src/Facade.php
@@ -37,17 +37,36 @@ class Facade {
     }
 
     /**
-     * @param string|int $key
-     * @param string|int $field 字段
-     * @param string|int $value 内容
-     * @param int        $ttl   有效时间
-     * @param bool       $force 存在时是否强制设置时间
+     * 设置
+     * @param string|int       $key
+     * @param string|int       $field 字段
+     * @param array|string|int $value 内容
+     * @param int              $ttl   有效时间
+     * @param bool             $force 存在时是否强制设置时间
      * @return bool
      */
-    public function hSet(string|int $key, string|int $field, string|int $value, int $ttl = 0, bool $force = false): bool {
+    public function hSet(string|int $key, string|int $field, array|string|int $value, int $ttl = 0, bool $force = false): bool {
+        $val = is_array($value) ? json_encode($value, JSON_UNESCAPED_UNICODE) : $value;
         return (bool) ($ttl > 0
-            ? $this->execLua("hSet", [(string) $key, (string) $field, (string) $value, $ttl, $force ? 1 : 0], 1)
-            : $this->client()->hSet((string) $key, (string) $field, (string) $value)
+            ? $this->execLua("hSet", [(string) $key, (string) $field, (string) $val, $ttl, $force ? 1 : 0], 1)
+            : $this->client()->hSet((string) $key, (string) $field, (string) $val)
         );
+    }
+
+    /**
+     * 获取
+     * @param string|int            $key
+     * @param array|string|int|null $fields
+     * @return array|null
+     */
+    public function hGet(string|int $key, array|string|int|null $fields = null): ?array {
+        $field = $fields ? (is_array($fields) ? $fields : [$fields]) : null;
+        $items = $field ? $this->client()->hMGet((string) $key, $field) : $this->client()->hGetAll((string) $key);
+        foreach ($items as $field => $value) {
+            if ($value[0] === '{' || $value[0] === '[') {
+                $items[$field] = json_decode($value, true);
+            }
+        }
+        return $items ?: null;
     }
 }
